318053bbd1899c6b2e07165425229551d8a1f93e
MJun-Kim <mj.kim@zipida.com>
2023-04-25 18:15:45 +0900

    Add Component and Modify style
    - add component
      - InputNumber
    - modify style
      - Select
      - Slider

---
+++ src/components/InputNumber/InputNumber.stories.ts
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import InputNumber from './InputNumber'
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
+const meta: Meta<typeof InputNumber> = {
+  title: 'Components/InputNumber',
+  component: InputNumber,
+  tags: ['autodocs'],
+  argTypes: {},
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const ButtonDisabled: Story = {
+  args: {
+    max: 1,
+    value: 0,
+    min: 0,
+  },
+}
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
---
+++ src/components/InputNumber/index.ts
@@ -0,0 +1,1 @@
+export { default as InputNumber, type InputNumberProps } from './InputNumber'
